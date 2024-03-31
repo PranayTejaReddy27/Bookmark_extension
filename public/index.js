6c8913d9dec4da39035bc5a670ffd55d598931f6
PranayTejaReddy27 <pranayteja270@gmail.com>
2024-03-31 19:43:46 +0530

    firstMain

---
+++ public/index.js
@@ -0,0 +1,14 @@
+// Get the dropdown content
+var dropdownContent = document.getElementById("myDropdown");
+
+// Hide the dropdown content by default
+dropdownContent.style.display = "none";
+
+// Toggle the visibility of the dropdown content when the image is clicked
+document.getElementById("pluse").onclick = function() {
+  if (dropdownContent.style.display === "none") {
+    dropdownContent.style.display = "block";
+  } else {
+    dropdownContent.style.display = "none";
+  }
+}
