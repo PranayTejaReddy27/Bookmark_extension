5cf7fef4c52e03e0b79132449a6222eb6542b66b
PranayTejaReddy27 <pranayteja270@gmail.com>
2024-04-02 23:28:25 +0530

    firstMain

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const bodyParser = require('body-parser');
 
 // Create MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'bookmarks',
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 });
 
 let clientIp = '';
