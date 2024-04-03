e6145f2dff01e77648f1c91a26fea23c25db16c0
PranayTejaReddy27 <pranayteja270@gmail.com>
2024-04-03 18:20:03 +0530

    firstMain

--- public/index.js
+++ public/index.js
@@ -1,17 +1,73 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Get the dropdown content
-    const dropdownContent = document.getElementById("myDropdown");
-  
-    // Hide the dropdown content by default
-    dropdownContent.style.display = "none";
-  
-    // Toggle the visibility of the dropdown content when the image is clicked
-    document.getElementById("pluse").onclick = function() {
-      if (dropdownContent.style.display === "none") {
-        dropdownContent.style.display = "block";
-      } else {
-        dropdownContent.style.display = "none";
-      }
-    };
+  // Get the dropdown content
+  const dropdownContent = document.getElementById("myDropdown");
 
-  });
+  // Hide the dropdown content by default
+  dropdownContent.style.display = "none";
+
+  // Toggle the visibility of the dropdown content when the image is clicked
+  document.getElementById("pluse").onclick = function() {
+    if (dropdownContent.style.display === "none") {
+      dropdownContent.style.display = "block";
+    } else {
+      dropdownContent.style.display = "none";
+    }
+  };
+
+  //Ip data to send
+  let clientIp = "";
+
+  function getPublicIPAddress() {
+    // Return a promise that resolves with the IP address
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.open('GET', 'https://api.ipify.org/?format=json');
+      request.onload = function() {
+        if (request.status === 200) {
+          const ipData = JSON.parse(request.responseText);
+          resolve(ipData.ip.replace(/\./g, '_'));
+        } else {
+          reject(Error('Failed to fetch IP address'));
+        }
+      };
+      request.onerror = function() {
+        reject(Error('Failed to fetch IP address'));
+      };
+      request.send();
+    });
+  }
+
+  // Call the function to get the IP address
+  getPublicIPAddress()
+    .then(ip => {
+      console.log('Public IP Address:', ip);
+      clientIp = '_' + ip;
+      console.log('clientIp :', clientIp)
+      
+      const dataToSend = {
+        clientIp: clientIp,
+      };
+
+      // Make a POST request to the server
+      fetch('/endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json' // Specify the content type
+        },
+        body: JSON.stringify(dataToSend) // Convert the data to JSON format
+      })
+      .then(response => {
+        if (response.ok) {
+          console.log('Data sent successfully');
+        } else {
+          console.error('Failed to send data');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    })
+    .catch(error => {
+      console.error('Error getting public IP address:', error);
+    });
+});
--- server.js
+++ server.js
@@ -13,94 +13,54 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT
 });
 
-let clientIp = '';
-
-// Middleware to retrieve client's IP address
-const http = require('http');
-
-app.use((req, res, next) => {
-  // Function to get the public IP address
-  function getPublicIPAddress() {
-    // Return a promise that resolves with the IP address
-    return new Promise((resolve, reject) => {
-      const options = {
-        hostname: 'api.ipify.org',
-        path: '/?format=json',
-        method: 'GET'
-      };
-
-      const request = http.request(options, response => {
-        let data = '';
-        response.on('data', chunk => {
-          data += chunk;
-        });
-        response.on('end', () => {
-          try {
-            const ipData = JSON.parse(data);
-            resolve(ipData.ip.replace(/\./g, '_'));
-          } catch (error) {
-            reject(error);
-          }
-        });
-      });
-
-      request.on('error', error => {
-        reject(error);
-      });
-
-      request.end();
-    });
-  }
-
-  // Call the function to get the IP address
-  getPublicIPAddress()
-    .then(ip => {
-      // Set the client's IP address
-      clientIp = '_' + ip;
-      // Check if table exists for the client's IP address
-      const query = `SHOW TABLES LIKE '${clientIp}'`;
-      connection.query(query, (error, results) => {
-        if (error) {
-          console.error('Error executing MySQL query:', error);
-          return;
-        }
-        if (results.length === 0) {
-          // Create a new table for the client's IP address
-          const createTableQuery = `CREATE TABLE IF NOT EXISTS ${clientIp} (
-            bookmarkId INT AUTO_INCREMENT PRIMARY KEY,
-            title VARCHAR(255),
-            url VARCHAR(255),
-            faviconLink VARCHAR(255),
-            tags VARCHAR(255),
-            category VARCHAR(255),
-            time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            favorite INT
-          )`;
-          connection.query(createTableQuery, (error, results) => {
-            if (error) {
-              console.error('Error creating table:', error);
-              return;
-            }
-            console.log(`Created new table for ${clientIp}`);
-          });
-        }
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching IP address:', error);
-    })
-    .finally(() => {
-      next(); // Call next middleware
-    });
-});
-
-
+let client_Ip = "";
+// Middleware to parse URL-encoded form data
+app.use(bodyParser.urlencoded({ extended: true }));
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+//new form entry
+app.use(express.json()); // Middleware to parse JSON bodies
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+app.post('/endpoint', (req, res) => {
+  const {clientIp} = req.body;
+  console.log('Received Ip data:', clientIp);
+  client_Ip = clientIp;
+  res.send('Data received successfully'); // Send a response back to the client
+  const query = `SHOW TABLES LIKE '${client_Ip}'`;
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.error('Error executing MySQL query:', error);
+      return;
+    }
+    if (results.length === 0) {
+      // Create a new table for the client's IP address
+      const createTableQuery = `CREATE TABLE IF NOT EXISTS ${clientIp} (
+        bookmarkId INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255),
+        url VARCHAR(255),
+        faviconLink VARCHAR(255),
+        tags VARCHAR(255),
+        category VARCHAR(255),
+        time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        favorite INT
+      )`;
+      connection.query(createTableQuery, (error, results) => {
+        if (error) {
+          console.error('Error creating table:', error);
+          return;
+        }
+        console.log(`Created new table for ${clientIp}`);
+      });
+    }
+  });
+});
+
+
 // Define a route to fetch data from MySQL and send it to the client
 app.get('/data', (req, res) => {
-  const query = `SELECT * FROM ${clientIp}`;
+  const query = `SELECT * FROM ${client_Ip}`;
   connection.query(query, (error, results) => {
     if (error) {
       console.error('Error executing MySQL query:', error);
@@ -111,12 +71,7 @@ app.get('/data', (req, res) => {
   });
 });
 
-// Middleware to parse URL-encoded form data
-app.use(bodyParser.urlencoded({ extended: true }));
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
-//new form entry
-app.use(express.json()); // Middleware to parse JSON bodies
+
 app.post('/submit-form', (req, res) => {
   // Extract form data from request body
   const { url, tags, category } = req.body;
@@ -129,8 +84,9 @@ app.post('/submit-form', (req, res) => {
   const data = {title, url,faviconLink,tags,category,favorite};
 
   // Perform database operation to insert the data into the specified table
-  const query = `INSERT INTO ${clientIp} SET ?`;
-  connection.query(query, data, (error, results) => {
+  const query = `INSERT INTO ?? SET ?`;
+  const values = [client_Ip, data];
+  connection.query(query, values, (error, results) => {
     if (error) {
       console.error('Error inserting data into database:', error);
       res.status(500).json({ error: 'Internal Server Error' });
@@ -144,4 +100,5 @@ app.post('/submit-form', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`ip_address ${client_Ip}`);
 });
